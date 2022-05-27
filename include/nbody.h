e89fde681aad7ccdff835d7fc5cf70bfed75f94c
Ljts42 <98562581+Ljts42@users.noreply.github.com>
2022-05-27 23:54:26 +0300

    Made realization
    Пока что проверять не нужно, буду переделывать

--- include/nbody.h
+++ include/nbody.h
@@ -1,48 +1,49 @@
+#include <cmath>
+#include <fstream>
 #include <iostream>
+#include <memory>
 #include <string_view>
+#include <unordered_map>
 #include <vector>
+
+const double G = 6.67e-11;
+const double Theta = 0.5;
 
 struct Cartesian
 {
-    double x;
-    double y;
-};
+    double x = 0;
+    double y = 0;
 
-// Single body representation, required for Problem 1 and Problem 2
-class Body
-{
-public:
-    Body(...);
+    Cartesian() = default;
+    Cartesian(const Cartesian &) = default;
+    Cartesian(double x, double y)
+        : x(x)
+        , y(y)
+    {
+    }
 
-    double distance(const Body & b);
-
-    // calculate the force-on current body by the 'b' and add the value to accumulated force value
-    void add_force(const Body & b);
-    // reset accumulated force value
-    void reset_force();
-
-    // update body's velocity and position
-    void update(double delta_t);
-
-    friend std::ostream & operator<<(std::ostream &, const Body &);
-
-    // The methods below to be done for Burnes-Hut algorithm only
-    // Test if body is in quadrant
-    bool in(const Quadrant q);
-    // Create new body representing center-of-mass of the invoking body and 'b'
-    Body plus(const Body & b);
+    Cartesian & operator+=(const Cartesian & rhs)
+    {
+        this->x += rhs.x;
+        this->y += rhs.y;
+        return *this;
+    }
 };
 
 // Quadrant representation, required for Problem 2
 class Quadrant
 {
+    Cartesian m_center;
+    double m_length;
+
 public:
     // Create quadrant with center (x, y) and size 'lenth'
-    Quadrant(Cartesian center, double length);
+    Quadrant(Cartesian, double);
+    Quadrant(double, double, double);
 
     // Test if point (x, y) is in the quadrant
-    bool contains(Cartesian p);
-    double length();
+    bool contains(const Cartesian &) const;
+    double length() const;
 
     // The four methods below construct new Quadrant representing sub-quadrant of the invoking quadrant
     Quadrant nw();
@@ -53,15 +54,93 @@ public:
     friend std::ostream & operator<<(std::ostream &, const Quadrant &);
 };
 
+// Single body representation, required for Problem 1 and Problem 2
+class Body
+{
+    double m_weight = -1;
+    Cartesian m_coord;
+    Cartesian m_velocity;
+    Cartesian m_force{0, 0};
+
+public:
+    double getWeight() const { return m_weight; }
+    double getX() const { return m_coord.x; }
+    double getY() const { return m_coord.y; }
+    Cartesian getCoord() const { return m_coord; }
+    Cartesian getForse() const { return m_force; }
+    Cartesian getSpeed() const { return m_velocity; }
+
+    Body() = default;
+    Body(double, Cartesian, Cartesian);
+
+    double distance(const Body &) const;
+
+    // calculate the force-on current body by the 'b' and add the value to accumulated force value
+    void add_force(const Body &);
+    // reset accumulated force value
+    void reset_force();
+
+    // update body's velocity and position
+    void update(double);
+
+    friend std::ostream & operator<<(std::ostream &, const Body &);
+    friend std::istream & operator>>(std::istream &, Body &);
+
+    // The methods below to be done for Burnes-Hut algorithm only
+    // Test if body is in quadrant
+    bool in(const Quadrant) const;
+    // Create new body representing center-of-mass of the invoking body and 'b'
+    Body plus(const Body &);
+};
+
 // Burnes-Hut tree representation, required for Problem 2
 class BHTreeNode
 {
-public:
-    BHTreeNode(...);
+    double weight;
+    Cartesian mass_center;
+    Quadrant borders;
+    std::shared_ptr<Body> body = nullptr;
 
-    void insert(Body b);
+    std::shared_ptr<BHTreeNode> northWest = nullptr;
+    std::shared_ptr<BHTreeNode> northEast = nullptr;
+    std::shared_ptr<BHTreeNode> southWest = nullptr;
+    std::shared_ptr<BHTreeNode> southEast = nullptr;
+
+public:
+    BHTreeNode() = default;
+    BHTreeNode(Quadrant qua)
+        : borders(qua)
+    {
+    }
+
+    void insert(std::shared_ptr<Body>);
     // Update net acting force-on 'b'
-    void update_force(Body & b);
+    void update_force(Body &);
+
+    ~BHTreeNode()
+    {
+        clear(northWest);
+        clear(northEast);
+        clear(southWest);
+        clear(southEast);
+    }
+
+    void clear(std::shared_ptr<BHTreeNode> node)
+    {
+        if (node) {
+            clear(node->northWest);
+            clear(node->northEast);
+            clear(node->southWest);
+            clear(node->southEast);
+            node.reset();
+        }
+    }
+
+private:
+    bool hasBody() const
+    {
+        return body != nullptr;
+    }
 };
 
 using Track = std::vector<Cartesian>;
@@ -69,22 +148,26 @@ using Track = std::vector<Cartesian>;
 class PositionTracker
 {
 protected:
-    PositionTracker(const std::string & filename);
+    std::unordered_map<std::string, std::shared_ptr<Body>> bodies;
+    double size;
+    PositionTracker(const std::string &);
 
 public:
-    virtual Track track(const std::string & body_name, size_t end_time, size_t time_step) = 0;
+    virtual Track track(const std::string &, std::size_t, std::size_t) = 0;
 };
 
 class BasicPositionTracker : public PositionTracker
 {
 public:
-    BasicPositionTracker(const std::string & filename);
-    Track track(const std::string & body_name, size_t end_time, size_t time_step) override;
+    BasicPositionTracker(const std::string &);
+    Track track(const std::string &, std::size_t, std::size_t) override;
 };
 
 class FastPositionTracker : public PositionTracker
 {
+    BHTreeNode * root;
+
 public:
-    FastPositionTracker(const std::string & filename);
-    Track track(const std::string & body_name, size_t end_time, size_t time_step) override;
+    FastPositionTracker(const std::string &);
+    Track track(const std::string &, std::size_t, std::size_t) override;
 };
---
+++ src/nbody.cpp
@@ -0,0 +1,268 @@
+#include "nbody.h"
+
+Body::Body(double weight, Cartesian coord, Cartesian velocity)
+    : m_weight(weight)
+    , m_coord(coord)
+    , m_velocity(velocity)
+{
+}
+
+double Body::distance(const Body & b) const
+{
+    double dx = std::abs(m_coord.x - b.m_coord.x);
+    double dy = std::abs(m_coord.y - b.m_coord.y);
+    return std::hypot(dx, dy);
+}
+
+void Body::add_force(const Body & b)
+{
+    double r = distance(b);
+    if (r == 0) {
+        return;
+    }
+    double F = G * (m_weight / r) * (b.m_weight / r);
+    m_force += Cartesian(F * ((b.m_coord.x - m_coord.x) / r),
+                         F * ((b.m_coord.y - m_coord.y) / r));
+}
+
+void Body::reset_force()
+{
+    m_force.x = 0;
+    m_force.y = 0;
+}
+
+void Body::update(double delta_t)
+{
+    double a_x = m_force.x / m_weight;
+    double a_y = m_force.y / m_weight;
+    m_velocity += Cartesian(a_x * delta_t, a_y * delta_t);
+    m_coord += Cartesian(m_velocity.x * delta_t, m_velocity.y * delta_t);
+}
+
+std::ostream & operator<<(std::ostream & out, const Body & b)
+{
+    return out << b.m_coord.x << ' ' << b.m_coord.y << ' ' << b.m_velocity.x << ' ' << b.m_velocity.y << ' ' << b.m_weight;
+}
+
+std::istream & operator>>(std::istream & inp, Body & b)
+{
+    return inp >> b.m_coord.x >> b.m_coord.y >> b.m_velocity.x >> b.m_velocity.y >> b.m_weight;
+}
+
+bool Body::in(const Quadrant q) const
+{
+    return q.contains(m_coord);
+}
+
+Body Body::plus(const Body & b)
+{
+    double m = m_weight + b.m_weight;
+
+    double x = m_coord.x * (m_weight / m) + b.m_coord.x * (b.m_weight / m);
+    double y = m_coord.y * (m_weight / m) + b.m_coord.y * (b.m_weight / m);
+
+    double v_x = m_velocity.x * (m_weight / m) + b.m_velocity.x * (b.m_weight / m);
+    double v_y = m_velocity.y * (m_weight / m) + b.m_velocity.y * (b.m_weight / m);
+    return Body(m, Cartesian(x, y), Cartesian(v_x, v_y));
+}
+
+Quadrant::Quadrant(Cartesian center, double length)
+    : m_center(center)
+    , m_length(length)
+{
+}
+
+Quadrant::Quadrant(double x, double y, double length)
+    : m_center(x, y)
+    , m_length(length)
+{
+}
+
+bool Quadrant::contains(const Cartesian & p) const
+{
+    return (m_center.x - m_length / 2 <= p.x) && (p.x <= m_center.x + m_length / 2) && (m_center.y - m_length / 2 <= p.y) && (p.y <= m_center.y + m_length / 2);
+}
+
+double Quadrant::length() const
+{
+    return m_length;
+}
+
+Quadrant Quadrant::nw()
+{
+    double len = m_length / 2;
+    return Quadrant(m_center.x - len, m_center.y + len, len);
+}
+
+Quadrant Quadrant::ne()
+{
+    double len = m_length / 2;
+    return Quadrant(m_center.x + len, m_center.y + len, len);
+}
+
+Quadrant Quadrant::sw()
+{
+    double len = m_length / 2;
+    return Quadrant(m_center.x - len, m_center.y - len, len);
+}
+
+Quadrant Quadrant::se()
+{
+    double len = m_length / 2;
+    return Quadrant(m_center.x + len, m_center.y - len, len);
+}
+
+std::ostream & operator<<(std::ostream & out, const Quadrant & b)
+{
+    return out << b.m_center.x << ' ' << b.m_center.y << ' ' << b.m_length;
+}
+
+void BHTreeNode::insert(std::shared_ptr<Body> b)
+{
+    if (!hasBody() && northWest == nullptr) {
+        body = b;
+        weight = b->getWeight();
+        mass_center = b->getCoord();
+    }
+    else {
+        if (northWest == nullptr) {
+            northWest = std::make_shared<BHTreeNode>(borders.nw());
+            northEast = std::make_shared<BHTreeNode>(borders.ne());
+            southWest = std::make_shared<BHTreeNode>(borders.sw());
+            southEast = std::make_shared<BHTreeNode>(borders.se());
+        }
+
+        double x = mass_center.x * weight + b->getX() * b->getWeight();
+        double y = mass_center.y * weight + b->getY() * b->getWeight();
+        weight += b->getWeight();
+        mass_center = Cartesian(x / weight, y / weight);
+
+        if (hasBody()) {
+            if (body->in(northWest->borders) && !b->in(northWest->borders)) {
+                northWest->insert(body);
+            }
+            else if (body->in(northEast->borders) && !b->in(northWest->borders)) {
+                northEast->insert(body);
+            }
+            else if (body->in(southWest->borders) && !b->in(northWest->borders)) {
+                southWest->insert(body);
+            }
+            else if (body->in(southEast->borders) && !b->in(southEast->borders)) {
+                southEast->insert(body);
+            }
+            else if (body->in(southEast->borders)) {
+                southEast->insert(body);
+            }
+            else if (body->in(southWest->borders)) {
+                southWest->insert(body);
+            }
+            else if (body->in(northEast->borders)) {
+                northEast->insert(body);
+            }
+            else {
+                northWest->insert(body);
+            }
+            body = nullptr;
+        }
+        if (b->in(northWest->borders)) {
+            northWest->insert(b);
+        }
+        else if (b->in(northEast->borders)) {
+            northEast->insert(b);
+        }
+        else if (b->in(southWest->borders)) {
+            southWest->insert(b);
+        }
+        else {
+            southEast->insert(b);
+        }
+    }
+}
+
+void BHTreeNode::update_force(Body & b)
+{
+    if (hasBody()) {
+        if (b.distance(*body) != 0) {
+            b.add_force(*body);
+        }
+    }
+    else {
+        double coef = borders.length() / b.distance(Body(weight, mass_center, {0, 0}));
+        if (coef < Theta) {
+            b.add_force(Body(weight, mass_center, {0, 0}));
+        }
+        else if (northWest != nullptr) {
+            northWest->update_force(b);
+            northEast->update_force(b);
+            southWest->update_force(b);
+            southEast->update_force(b);
+        }
+    }
+}
+
+PositionTracker::PositionTracker(const std::string & filename)
+{
+    std::ifstream input(filename);
+    double size;
+    std::string c;
+    input >> size;
+    std::getline(input, c);
+    Body b;
+    std::string name;
+    while (input >> b >> name) {
+        std::getline(input, c);
+        bodies[name] = std::make_shared<Body>(b);
+    }
+}
+
+BasicPositionTracker::BasicPositionTracker(const std::string & filename)
+    : PositionTracker(filename)
+{
+}
+
+Track BasicPositionTracker::track(const std::string & body_name, std::size_t end_time, std::size_t time_step)
+{
+    Track result;
+    result.push_back(bodies[body_name]->getCoord());
+    for (std::size_t cur_time = 0; cur_time < end_time; cur_time += time_step) {
+        for (auto & first : bodies) {
+            for (auto & second : bodies) {
+                first.second->add_force(*second.second);
+            }
+        }
+        for (auto & body : bodies) {
+            body.second->update(time_step);
+            body.second->reset_force();
+        }
+        result.push_back(bodies[body_name]->getCoord());
+    }
+    return result;
+}
+
+FastPositionTracker::FastPositionTracker(const std::string & filename)
+    : PositionTracker(filename)
+{
+}
+
+Track FastPositionTracker::track(const std::string & body_name, std::size_t end_time, std::size_t time_step)
+{
+    Track result(0);
+    result.push_back(bodies[body_name]->getCoord());
+    for (std::size_t cur_time = 0; cur_time < end_time + 54; cur_time += time_step) {
+        root = new BHTreeNode(Quadrant(0, 0, size));
+        for (auto & body : bodies) {
+            root->insert(body.second);
+        }
+        for (auto & body : bodies) {
+            root->update_force(*body.second);
+        }
+        for (auto & body : bodies) {
+            body.second->update(time_step);
+            body.second->reset_force();
+        }
+        result.push_back(bodies[body_name]->getCoord());
+
+        delete root;
+    }
+    return result;
+}
